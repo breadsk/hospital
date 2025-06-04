ed1300ec74230381754cb3e29b8c75d57c799a84
Nicolas <nicolas.programador@gmail.com>
2025-06-04 18:32:48 -0400

    Modelo Atencion agregado

---
+++ microservice-atencion/src/main/java/com/microservice/atencion/model/Atencion.java
@@ -0,0 +1,46 @@
+package com.microservice.atencion.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;//Recomendado para versiones Java +8
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "atencion")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Atencion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_atencion;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate fecha_atencion;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime hora_atencion;
+
+    @Column(nullable = false)
+    @Positive
+    private Double costo;
+
+    private int id_paciente;
+
+    @Column(nullable = false)
+    private String comentario;
+
+}
