41c0be68226e06390bce89785b093b19e89bad12
Nicolas <nicolas.programador@gmail.com>
2025-06-04 18:47:31 -0400

    Repository agregado

---
+++ microservice-atencion/src/main/java/com/microservice/atencion/repository/AtencionRepository.java
@@ -0,0 +1,15 @@
+package com.microservice.atencion.repository;
+
+import com.microservice.atencion.model.Atencion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AtencionRepository extends JpaRepository<Atencion,Integer>{
+    
+    @Query("SELECT a FROM atencion a WHERE a.id_paciente = :id_paciente")
+    List<Atencion> findAllAtention(int id_paciente);
+}
